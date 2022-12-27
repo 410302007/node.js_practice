8e0a34176a496fc3e2783e74da1aca7cad2860bc
ChuYaHsu <t199511137@gmail.com>
2022-12-27 11:30:37 +0800

    http-server01

---
+++ src/http_server01.js
@@ -0,0 +1,13 @@
+
+const http = require('http');  //直接打套件名稱
+
+const server = http.createServer((request, response)=>{          //request 、 response 順序不能顛倒
+  response.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  response.end(`<h2>Hello</h2>
+  <p>${request.url}</p>`
+  )
+});
+
+server.listen(3000);
