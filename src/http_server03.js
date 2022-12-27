7602538d78061a27ada38f42fa88d9b880368164
ChuYaHsu <t199511137@gmail.com>
2022-12-27 13:47:11 +0800

    讀取檔案

--- src/http_server03.js
+++ src/http_server03.js
@@ -3,11 +3,12 @@ const http = require('http');  //直接打套件名稱
 const fs = require('fs/promises'); //使用Promise API
 
 const server = http.createServer(async(req, res)=>{          //request 、 response 順序不能顛倒
- const no_return_data = await fs.writeFile(                     //async await -> 使用try catch 除錯
-  __dirname + '/header01.txt', 
-  JSON.stringify(req. headers, null , 4));
-
-  res.end(`<h2>error: ${no_return_data}</h2>`);
+  console.log(`------------`, req.url);
+  const result = await fs.readFile(                         //error -> array
+ __dirname + "/esm01.html"
+ );
+  console.log(result.toString());
+  res.end(result.toString());
  });
 
 
