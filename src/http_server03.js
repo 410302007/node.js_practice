093b6da185b976a79cc95844e5676295d962e866
ChuYaHsu <t199511137@gmail.com>
2022-12-27 13:53:57 +0800

    readFile 2

--- src/http_server03.js
+++ src/http_server03.js
@@ -4,8 +4,11 @@ const fs = require('fs/promises'); //使用Promise API
 
 const server = http.createServer(async(req, res)=>{          //request 、 response 順序不能顛倒
   console.log(`------------`, req.url);
+  res.writeHead(200,{
+    'Content-Type': 'text/plain'
+  })
   const result = await fs.readFile(                         //error -> array
- __dirname + "/esm01.html"
+ __dirname + "/header01.txt"
  );
   console.log(result.toString());
   res.end(result.toString());
