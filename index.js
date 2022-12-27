f3a3bad048e11b8ee4a028fe0c24ef041881062c
ChuYaHsu <t199511137@gmail.com>
2022-12-27 15:17:36 +0800

    404 page

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ app.get('/',(req, res)=>{
   res.send(`<h1>您好</h1>`);
 
 });
+//!!所有的路由設定都要放在這行之前!!
+app.use((req, res)=>{
+  res.type("text/html");
+  res.status(404).send(`<h1>404找不到你要的頁面</h1>`);
+
+});
+
 const port = process.env.PORT || 3001;  //沒有抓到值(PORT=3002)， 選擇3001
 app.listen(port, ()=>{
   console.log(`server started:${port}`); //啟動server port:3002
