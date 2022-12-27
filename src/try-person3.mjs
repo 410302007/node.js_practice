ba1a3be6b88cc48d0f4bc656bc7cc60e28f091f5
ChuYaHsu <t199511137@gmail.com>
2022-12-27 10:52:10 +0800

    增加註解

--- src/try-person3.mjs
+++ src/try-person3.mjs
@@ -6,4 +6,4 @@ const p1 = new Person('David', 25);
 
 console.log(p1.toString());    //#2.{"name":"David","age":25}
 console.log(f2(8));    //#3. 81
-console.log(f3(8));
+console.log(f3(8));    //#4. 512
