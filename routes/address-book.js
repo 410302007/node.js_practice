58e691805c08eff514e5b443a1e214accd4da027
ChuYaHsu <t199511137@gmail.com>
2022-12-29 15:15:14 +0800

    num : totalRows

--- routes/address-book.js
+++ routes/address-book.js
@@ -6,8 +6,8 @@ const router =  express.Router();
 
 //求總筆數
 router.get('/', async(req,res)=>{
-  const t_sql ="SELECT COUNT(1) totalRows From address_book";
-  const [[{totalRows}]] = await db.query(t_sql);       //[[{totalRows}]]
+  const t_sql ="SELECT COUNT(1) num From address_book";
+  const [[{num: totalRows}]] = await db.query(t_sql);       //[[{totalRows}]]
   res.json(totalRows);
 });
 module.exports = router; 
